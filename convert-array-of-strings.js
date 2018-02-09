9ae8d95bab6ea3fb0f96d9ffe0c3a69211399b2e
Brian Foley <bfoley-teamug@users.noreply.github.com>
2018-02-09 07:28:05 -0800

    Convert an array of strings to array of numbers
    Convert an array of strings to array of numbers codewars challengs

---
+++ convert-array-of-strings.js
@@ -0,0 +1,11 @@
+//codewars Convert an array of strings to array of numbers
+
+var stringarray = ['1', '2', '3'];
+
+function toNumberArray(stringarray) {
+return stringarray.map(Number);
+
+}
+
+
+console.log(toNumberArray(stringarray));
